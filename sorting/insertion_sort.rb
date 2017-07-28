362c09c846c1627cc598e30ea5cafa87955d3de7
Prasanth Kommini <pkommini@salesforce.com>
2017-07-27 17:52:18 -0700

    Add before and after array print

--- sorting/insertion_sort.rb
+++ sorting/insertion_sort.rb
@@ -1,26 +1,37 @@
 #!/usr/bin/env ruby
 
 def insertion_sort(data)
-  1.upto(data.count - 1) do |i|
-    i.downto(1) do |j|
-      break if data[j] >= data[j - 1]
-      data[j], data[j - 1] = data[j - 1], data[j]
+  (1...data.count).each do |i|
+    j = i - 1
+    element = data[i]
+    while element < data[j] && j >= 0
+      data[j + 1] = data[j]
+      j -= 1
     end
+    data[j + 1] = element
   end
 end
 
 data = [11, 0, 10, 1, 9, 2, 8, 3, 7, 4, 6, 5]
+puts "Before Sort: #{data}"
 insertion_sort(data)
-puts "Data is: #{data}"
+puts "After Sort: #{data}"
+puts "\n"
 
 data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 10]
+puts "Before Sort: #{data}"
 insertion_sort(data)
-puts "Data is: #{data}"
+puts "After Sort: #{data}"
+puts "\n"
 
 data = [0, 1, 2, 2, 3, 4, 4, 7, 8, 9, 11, 10]
+puts "Before Sort: #{data}"
 insertion_sort(data)
-puts "Data is: #{data}"
+puts "After Sort: #{data}"
+puts "\n"
 
 data = [1, 0, 3, 2, 4, 3, 6, 5, 9, 8, 11, 10]
+puts "Before Sort: #{data}"
 insertion_sort(data)
-puts "Data is: #{data}"
+puts "After Sort: #{data}"
+puts "\n"
--- sorting/selection_sort.rb
+++ sorting/selection_sort.rb
@@ -1,24 +1,35 @@
 #!/usr/bin/env ruby
 
-# --------------------------------------------------------------------------------
-# Selection Sort
-# --------------------------------------------------------------------------------
 def selection_sort(data)
-  n = data.count
+  n = data.count - 1
 
-  0.upto(n - 1) do |i|
+  0.upto(n) do |i|
     min_idx = i
-    (i + 1).upto(n - 1) { |j| min_idx = j if data[j] < data[min_idx] }
-    # puts "data is: #{data}"; puts "data[i] is #{data[i]}"; puts "data[min_idx] is #{data[min_idx]}"
-    data[i], data[min_idx] = data[min_idx], data[i] # unless min.eql?(i)
+    (i + 1).upto(n) { |j| min_idx = j if data[j] < data[min_idx] }
+    data[i], data[min_idx] = data[min_idx], data[i] unless min_idx.eql?(i)
   end
 end
 
-
 data = [11, 0, 10, 1, 9, 2, 8, 3, 7, 4, 6, 5]
+puts "Before Sort: #{data}"
 selection_sort(data)
-puts "Data is: #{data}"
+puts "After Sort: #{data}"
+puts "\n"
 
 data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 10]
+puts "Before Sort: #{data}"
 selection_sort(data)
-puts "Data is: #{data}"
+puts "After Sort: #{data}"
+puts "\n"
+
+data = [0, 1, 2, 2, 3, 4, 4, 7, 8, 9, 11, 10]
+puts "Before Sort: #{data}"
+selection_sort(data)
+puts "After Sort: #{data}"
+puts "\n"
+
+data = [1, 0, 3, 2, 4, 3, 6, 5, 9, 8, 11, 10]
+puts "Before Sort: #{data}"
+selection_sort(data)
+puts "After Sort: #{data}"
+puts "\n"
