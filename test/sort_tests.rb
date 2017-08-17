e4689e52b53cde6b31f1b14264f7e83d3bff3d63
Prasanth Kommini <pkommini@salesforce.com>
2017-08-16 20:20:28 -0700

    Unit test suite

---
+++ test/sort_tests.rb
@@ -0,0 +1,62 @@
+#!/usr/bin/env ruby
+
+Dir['../sorting/*.rb'].each do |file|
+  require_relative File.join(File.dirname(file), File.basename(file, File.extname(file)))
+end
+require 'test/unit'
+require 'set'
+
+class SortTests < Test::Unit::TestCase
+  def rand_n(n, max)
+    randoms = Set.new
+    loop do
+      randoms << rand(max)
+      return randoms.to_a if randoms.size >= n
+    end
+  end
+
+  def test_merge_sort
+    assert_equal([1, 2, 3, 4, 5, 6, 7, 8], merge_sort([5, 6, 7, 8, 1, 2, 3, 4]))
+    assert_equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], merge_sort([11, 0, 10, 1, 9, 2, 8, 3, 7, 4, 6, 5]))
+    assert_equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], merge_sort([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]))
+    assert_equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], merge_sort([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]))
+    assert_equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], merge_sort([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]))
+    assert_equal(rand_n(1_000_000, 1_000_000).sort, merge_sort(rand_n(1_000_000, 1_000_000)))
+  end
+
+  def test_insertion_sort
+    assert_equal([1, 2, 3, 4, 5, 6, 7, 8], insertion_sort([5, 6, 7, 8, 1, 2, 3, 4]))
+    assert_equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], insertion_sort([11, 0, 10, 1, 9, 2, 8, 3, 7, 4, 6, 5]))
+    assert_equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], insertion_sort([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]))
+    assert_equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], insertion_sort([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]))
+    assert_equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], insertion_sort([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]))
+    assert_equal(rand_n(1_000_000, 1_000_000).sort, insertion_sort(rand_n(1_000_000, 1_000_000)))
+  end
+
+  def test_selection_sort
+    assert_equal([1, 2, 3, 4, 5, 6, 7, 8], selection_sort([5, 6, 7, 8, 1, 2, 3, 4]))
+    assert_equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], selection_sort([11, 0, 10, 1, 9, 2, 8, 3, 7, 4, 6, 5]))
+    assert_equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], selection_sort([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]))
+    assert_equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], selection_sort([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]))
+    assert_equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], selection_sort([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]))
+    assert_equal(rand_n(1_000_000, 1_000_000).sort, selection_sort(rand_n(1_000_000, 1_000_000)))
+  end
+
+  def test_heap_sort
+    assert_equal([1, 2, 3, 4, 5, 6, 7, 8], heap_sort([5, 6, 7, 8, 1, 2, 3, 4]))
+    assert_equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], heap_sort([11, 0, 10, 1, 9, 2, 8, 3, 7, 4, 6, 5]))
+    assert_equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], heap_sort([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]))
+    assert_equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], heap_sort([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]))
+    assert_equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], heap_sort([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]))
+    assert_equal(rand_n(1_000_000, 1_000_000).sort, heap_sort(rand_n(1_000_000, 1_000_000)))
+  end
+
+  def test_quick_sort
+    assert_equal([1, 2, 3, 4, 5, 6, 7, 8], quick_sort([5, 6, 7, 8, 1, 2, 3, 4]))
+    assert_equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], quick_sort([11, 0, 10, 1, 9, 2, 8, 3, 7, 4, 6, 5]))
+    assert_equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], quick_sort([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]))
+    assert_equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], quick_sort([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]))
+    assert_equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], quick_sort([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]))
+    assert_equal(rand_n(1_000_000, 1_000_000).sort, quick_sort(rand_n(1_000_000, 1_000_000)))
+  end
+end
